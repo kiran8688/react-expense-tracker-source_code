94cc50a075ce8a1915b1d834a640f18c5ba8a0f2
KIRAN_KUMAR SINGARAM <15j41a05p0@gmail.com>
2022-06-20 04:24:56 +0530

    completed: state, setState, map(render) incomplete: HOA method for the state(data)

--- src/Components/LeftSideComponents/Input.jsx
+++ src/Components/LeftSideComponents/Input.jsx
@@ -5,35 +5,50 @@ import './Input.css'
 import DisplaySec1 from './RightSideComponents/DisplaySec1';
 import DisplaySec2 from './RightSideComponents/DisplaySec2';
 export class Input extends Component {
-  
-  constructor(props){
+
+  constructor(props) {
     super(props)
-    this.state ={
-      tracker: [ 
-        {
-           id:1,
-          transactionName: null,
-          transactionAmount: null,
-          transactionDate: null
-        }
-    ]
+    this.state = {
+      tracker: []
+    }
   }
-  }
+
+  //   const currentConversation = state.conversation;
+  // currentConversation.push((<div>new div</div>));
+  // this.setState({conversation: currentConversation})
 
   inputClickEventHandler = () => {
-    
-    
-    
-    
-    this.setState({transactionName : document.querySelector('#name-input').value})
-    this.setState({transactionAmount : document.querySelector('#amount-input').value})
-    this.setState({transactionDate : document.querySelector('#date-input').value})
 
+
+
+    var transactions = [...this.state.tracker]
+
+    transactions.push({ id: Math.random(), transactionName: document.querySelector('#name-input').value, transactionAmount: document.querySelector('#amount-input').value, transactionDate: document.querySelector('#date-input').value })
+
+
+
+    this.setState({ tracker: transactions })
+    // this.setState({
+    //   tracker :[
+    //   {id: Math.random(), transactionName : document.querySelector('#name-input').value, transactionAmount : document.querySelector('#amount-input').value, transactionDate : document.querySelector('#date-input').value},
+
+    //   ]
+    // })
+
+    // this.state.tracker.push(this.setState.tracker) = transactions
+    console.log(transactions)
+    // var tracker = this.state.tracker
+    //  this.tracker.push(transact)
+    //  console.log(this.tracker);
+
+    // document.querySelector('#name-input').value=null, document.querySelector('#amount-input').value=null, document.querySelector('#date-input').value=null; 
   }
+
+
   takeNameHandler = (e) => {
 
     // this.setState( this.state.tracker[0].transactionName =  e.target.value)
-     console.log(e.target.value)
+    console.log(e.target.value)
 
   }
   takeAmountHandler = (e) => {
@@ -48,39 +63,73 @@ export class Input extends Component {
 
   render(props) {
 
+    if (this.state.tracker === []) {
+      var transact = (<center><br /><br /> Please add details of transaction</center>)
+    }
+
+    if (this.state.tracker !== "") {
+
+      transact =
+        this.state.tracker.map((transaction, index) => {
+
+          return (
+
+            <DisplaySec2 key={this.state.tracker[index].id} dispName={this.state.tracker[index].transactionName} dispAmount={this.state.tracker[index].transactionAmount} dispDate={this.state.tracker[index].transactionDate} />
+
+
+          )
+
+        })
+
+    }
+    // const convdata = this.state.tracker[0].transactionAmount
+    // console.log(res)
+    const mapAmt = this.state.tracker.map(totAmt => totAmt.transactionAmount)
+    const parseIntAmt = parseInt(mapAmt)
+    const reduceAmt = parseIntAmt.reduce((a, b) => (a += b), 0)
+    // const totalAmt = reduceAmt.Math.trunk()
+    // console.log(totalAmt)
+    console.log(typeof(reduceAmt))
+
     return (
       <>
-       <div className='row'>
-      <div id='input-head' className='col-3 ms-1 text-dark text-start bg-white'><br /><br /><br /><br />
-        <div className='container '><u className='opacity-75'><em> Add New Transaction Here :</em></u></div>
-        <br /><br />
-        <div className="h4 container-fluid ms-2 " >Transaction Name:  <input type="text" id='name-input' className='form-control' placeholder='Enter Transaction Name'  onChange={ this.takeNameHandler } />
-        </div>
-        <br /><br /><br />
+        <div className='row'>
+          <div id='input-head' className='col-3 ms-1 text-dark text-start bg-white'><br /><br /><br /><br />
+            <div className='container '><u className='opacity-75'><em> Add New Transaction Here :</em></u></div>
+            <br /><br />
+            <div className="h4 container-fluid ms-2 " >Transaction Name:  <input type="text" id='name-input' className='form-control' placeholder='Enter Transaction Name' onChange={this.takeNameHandler} />
+            </div>
+            <br /><br /><br />
 
-        <div className="h4 container-fluid ms-2" >Enter Amount : <label className='fs-6 opacity-75'>(Add <strong className='fs-3 text-success ' >+</strong> if its <span className='text-success'>INCOME</span>     ,    Add <strong className='fs-2 text-danger' >-</strong> if its <span className='text-danger'>Expense</span>)</label><input type="number" id='amount-input' className='form-control' placeholder='Enter Transaction Amount' onChange={this.takeAmountHandler} />
-        </div>
-        <br /><br /><br />
-        <div className="h4 container-fluid ms-2 " >Date of Transaction :  <input type="date" id='date-input' className='form-control' onChange={this.takeDateHandler} />
-        </div>
-        <br /><br /><br />
-        <button type='submit' className=' btn btn-primary ms-1 container-fluid  text-white border-white rounded-5 form-control' onClick={this.inputClickEventHandler}  >submit</button>
-        <br /><br /><br /><br /> <br /><br /><br /><br />
-
-
+            <div className="h4 container-fluid ms-2" >Enter Amount : <label className='fs-6 opacity-75'>(Add <strong className='fs-3 text-success ' >+</strong> if its <span className='text-success'>INCOME</span>     ,    Add <strong className='fs-2 text-danger' >-</strong> if its <span className='text-danger'>Expense</span>)</label><input type="number" id='amount-input' className='form-control' placeholder='Enter Transaction Amount' onChange={this.takeAmountHandler} />
+            </div>
+            <br /><br /><br />
+            <div className="h4 container-fluid ms-2 " >Date of Transaction :  <input type="date" id='date-input' className='form-control' onChange={this.takeDateHandler} />
+            </div>
+            <br /><br /><br />
+            <button type='submit' className=' btn btn-primary ms-1 container-fluid  text-white border-white rounded-5 form-control' onClick={this.inputClickEventHandler}  >submit</button>
+            <br /><br /><br /><br /> <br /><br /><br /><br />
 
 
-      </div>
+          </div>
 
-      <div className='col-auto h1 text-black '>
+          <div className='col-auto h1 text-black '>
             <div id='vr' className="vr "></div>
           </div>
 
           <div className='col-8'>
-            <DisplaySec1  />
-            <DisplaySec2 value={this.state.tracker[0].transactionName} />
+            <DisplaySec1 />
+
+            <div className='mt-4'>
+              <hr /> <span className='fs-2'>History  :</span>
+
+              <br /><br />
+
+              {transact}
+            </div>
+            {/* <DisplaySec2 dispName={this.state.tracker[0].transactionName} dispAmount={this.state.tracker[0].transactionAmount} dispDate={this.state.tracker[0].transactionDate} /> */}
           </div>
-          </div>
+        </div>
       </>
     )
   }
--- src/Components/LeftSideComponents/RightSideComponents/DisplaySec2.jsx
+++ src/Components/LeftSideComponents/RightSideComponents/DisplaySec2.jsx
@@ -3,18 +3,18 @@ import React, { Component } from 'react'
 export class DisplaySec2 extends Component {
   render(props) {
     return (
-      <div className='mt-4'>
-        <hr /> <span className='fs-2'>History  :</span>
+      // <div className='mt-4'>
+      //   <hr /> <span className='fs-2'>History  :</span>
 
-        <br /><br />
-        <div className=' fs-3'>
+      //   <br /><br />
+        <div className=' fs-4 container mb-4'>
             <ul className=' container-auto  bg-white '>
-            <li className=' row justify-content-around rounded border-end border-5 border-success mb-3 p-2 list-group-item shadow d-flex bg-light '> <strong className=' text-truncate col-6'>hello</strong> <span className='col-3 text-secondary'>10/12/2022</span> <span className='col-3'>+$50</span> </li>
-            <li className=' row justify-content-around rounded border-end border-5 border-danger p-2 mb-3 list-group-item shadow d-flex bg-light'><strong className='text-truncate col-6' >movie making</strong> <span className='col-3 text-secondary'>10/12/2022</span> <span className='col-3'>-$50000</span></li>
-            <li className=' row justify-content-around rounded border-end border-5 border-success p-2 mb-3 list-group-item shadow d-flex bg-light '> <strong className='text-truncate col-6'>shopping</strong> <span className='col-3 text-secondary'>10/12/2022</span> <span className='col-3'>+$900</span></li>
+            <li className=' row justify-content-around rounded border-end border-5 border-success mb-3 p-2 list-group-item shadow-lg d-flex bg-light '  > <strong className=' text-truncate col-6'>{this.props.dispName}</strong> <span className='col-3 text-secondary'>{this.props.dispDate}</span> <span className='col-3 text-truncate'>{this.props.dispAmount}</span> </li>
+            {/* <li className=' row justify-content-around rounded border-end border-5 border-danger p-2 mb-3 list-group-item shadow d-flex bg-light'><strong className='text-truncate col-6' >movie making</strong> <span className='col-3 text-secondary'>10/12/2022</span> <span className='col-3'>-$50000</span></li>
+            <li className=' row justify-content-around rounded border-end border-5 border-success p-2 mb-3 list-group-item shadow d-flex bg-light '> <strong className='text-truncate col-6'>shopping</strong> <span className='col-3 text-secondary'>10/12/2022</span> <span className='col-3'>+$900</span></li> */}
             </ul>
         </div>
-      </div>
+      // </div>
     )
   }
 }
